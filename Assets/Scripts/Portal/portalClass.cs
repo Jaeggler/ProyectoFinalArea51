dbe657c9241280e7fa6f849171fb17b1b09367c6
Luis Jose Regis Benavides <ljregisb@gmail.com>
2016-07-14 12:29:13 -0500

    Portal Controller

---
+++ Assets/Scripts/Portal/portalClass.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class portalClass {
+    public GameObject _monster;
+    public float _delay;
+    public int _quantity;
+}
